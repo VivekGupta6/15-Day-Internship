0e9cc7247d99df9b4c32fc8a498ff925bf4576ff
VivekGupta6 <75849583+VivekGupta6@users.noreply.github.com>
2021-06-03 15:40:36 +0530

    Vertical Scroll view Layout
    this is an xml file of Vertical Scroll View

--- MainActivity.java
+++ MainActivity.java
@@ -1,50 +1,58 @@
-package com.example.gym_splits;
+package com.example.webview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer mediaPlayer;
-    @Override
 
+    Button lv,gv,wv,hs;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
-        mediaPlayer= MediaPlayer.create(MainActivity.this,R.raw.letsgo);
-        mediaPlayer.start();
-        Thread thread=new Thread(){
-            public void run(){
-                try {
-                    sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }finally {
-                    Intent intent= new Intent(MainActivity.this,MainActivity2.class);
-                    startActivity(intent);
 
-                }
+        lv=findViewById(R.id.lv);
+        gv=findViewById(R.id.gv);
+        wv=findViewById(R.id.wv);
+        hs=findViewById(R.id.hs);
+
+        lv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),listView.class);
+                startActivity(intent);
             }
-        };
-        thread.start();
-    }
+        });
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mediaPlayer.release();
-        finish();
+        gv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2=new Intent(getApplicationContext(),GridView.class);
+                startActivity(intent2);
+            }
+        });
+        wv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3=new Intent(getApplicationContext(),WebView.class);
+                startActivity(intent3);
+            }
+        });
+
+        hs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent4=new Intent(getApplicationContext(),HorizontalScroll.class);
+                startActivity(intent4);
+            }
+        });
+
+
     }
 }
